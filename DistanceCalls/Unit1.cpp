4132bdb3203c827f8ce84dbbbb99882dd4ef4538
ButkovskiyLev <levbut@gmail.com>
2018-10-25 15:10:00 +0500

    Calls #1.0.2
    -some new buttons
    -variables

--- DistanceCalls/Unit1.cpp
+++ DistanceCalls/Unit1.cpp
@@ -8,9 +8,40 @@
 #pragma package(smart_init)
 #pragma resource "*.dfm"
 TForm1 *Form1;
+	int DistanceValue, timeValue;
+	float priceValue, CostValue;
+	void switchDistance(int Distance);
 //---------------------------------------------------------------------------
 __fastcall TForm1::TForm1(TComponent* Owner)
 	: TForm(Owner)
 {
+
 }
 //---------------------------------------------------------------------------
+
+void __fastcall TForm1::Button1Click(TObject *Sender)
+{
+
+	DistanceValue = 0;
+	timeValue = 0;
+	CostValue = 0;
+	DistanceValue = StrToInt(DistanceBetweenCities->Text);
+	timeValue = StrToInt(CallTime->Text);
+	switchDistance(DistanceValue);
+    Price->Caption = FloatToStrF(priceValue, ffCurrency, 6, 2);
+	Cost->Caption = FloatToStrF(priceValue*timeValue, ffCurrency, 6, 2);
+}
+
+void switchDistance(int Distance){
+
+	switch(Distance){
+		if (Distance <= 100) priceValue = 2.15;
+		else if (Distance > 100 && Distance <= 600) priceValue = 3.75;
+		else if (Distance > 600 && Distance <= 1200) priceValue = 4.13;
+		else if (Distance > 1200 && Distance <= 3000) priceValue = 4.00;
+		else if (Distance > 3000 && Distance <= 5000) priceValue = 5.55;
+		else if (Distance > 5000) priceValue = 6.15;
+	}
+}
+//---------------------------------------------------------------------------
+
--- DistanceCalls/Unit1.h
+++ DistanceCalls/Unit1.h
@@ -11,6 +11,8 @@
 #include <Vcl.ActnMan.hpp>
 #include <Vcl.Menus.hpp>
 #include <Vcl.ComCtrls.hpp>
+#include <Vcl.ExtCtrls.hpp>
+#include <Vcl.Imaging.jpeg.hpp>
 //---------------------------------------------------------------------------
 class TForm1 : public TForm
 {
@@ -18,15 +20,26 @@ __published:	// IDE-managed Components
 	TComboBox *ComboBox1;
 	TLabel *LabelOutAbonent;
 	TLabel *LabelIncomingAbonent;
-	TLabel *LabelCountry;
-	TLabel *Label1;
+	TLabel *LabelOutCountry;
+	TLabel *LabelInCountry;
 	TComboBox *ComboBox2;
 	TLabel *LabelCallTime;
-	TEdit *InputCallTime;
+	TEdit *CallTime;
 	TLabel *LabelMinutes;
 	TComboBox *ComboBox3;
 	TComboBox *ComboBox4;
 	TDateTimePicker *DateTimePicker1;
+	TLabel *LabelDistanceBetweenCities;
+	TEdit *DistanceBetweenCities;
+	TLabel *LabelKilometrs;
+	TLabel *LabelCost;
+	TLabel *Cost;
+	TButton *Button1;
+	TLabel *LabelPrice;
+	TLabel *Price;
+	TImage *Image1;
+	void __fastcall Button1Click(TObject *Sender);
+
 private:	// User declarations
 public:		// User declarations
 	__fastcall TForm1(TComponent* Owner);
